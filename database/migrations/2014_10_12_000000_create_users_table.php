8af1b3b6936bd81889af9202ac0ca08d552de4d4
Evgeny Tkachenko <tkachenko@cmsplanet.ru>
2018-10-27 12:18:46 +0300

    Avatars

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -30,6 +30,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Петр',
             'last_name' => 'Слепаков',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-1.png',
             'phone' => '79000000001',
             'type' => 1,
             'password' => \Hash::make('secret'),
@@ -40,6 +41,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Дмитрий',
             'last_name' => 'Дорохов',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-2.png',
             'phone' => '79000000002',
             'type' => 1,
             'password' => \Hash::make('secret'),
@@ -50,6 +52,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Генадий',
             'last_name' => 'Шишелов',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-3.png',
             'phone' => '79000000003',
             'type' => 1,
             'password' => \Hash::make('secret'),
@@ -60,6 +63,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Евгений',
             'last_name' => 'Васильев',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-4.png',
             'phone' => '79000000004',
             'type' => 1,
             'password' => \Hash::make('secret'),
@@ -70,6 +74,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Сергей',
             'last_name' => 'Логинов',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-5.png',
             'phone' => '79000000005',
             'type' => 1,
             'parent_id' => 14,
@@ -81,6 +86,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Алексей',
             'last_name' => 'Ротманов',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-6.png',
             'phone' => '79000000006',
             'type' => 1,
             'password' => \Hash::make('secret'),
@@ -91,6 +97,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Борис',
             'last_name' => 'Метельников',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-7.png',
             'phone' => '79000000007',
             'type' => 1,
             'password' => \Hash::make('secret'),
@@ -101,6 +108,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Максим',
             'last_name' => 'Матвеев',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-8.png',
             'phone' => '79000000008',
             'type' => 1,
             'password' => \Hash::make('secret'),
@@ -111,6 +119,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Артем',
             'last_name' => 'Широков',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-9.png',
             'phone' => '79000000009',
             'type' => 1,
             'password' => \Hash::make('secret'),
@@ -121,6 +130,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Олег',
             'last_name' => 'Зеленцов',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-10.png',
             'phone' => '79000000010',
             'type' => 1,
             'password' => \Hash::make('secret'),
@@ -138,6 +148,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Борис',
             'last_name' => 'Никифоров',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-11.png',
             'phone' => '79000000012',
             'type' => 2,
             'password' => \Hash::make('secret'),
@@ -148,6 +159,7 @@ class CreateUsersTable extends Migration
         DB::table('users')->insert([
             'first_name' => 'Аркадий',
             'last_name' => 'Носонов',
+            'avatar' => 'http://tester1.evgenytk.ru/avatar-12.png',
             'phone' => '79000000013',
             'type' => 2,
             'password' => \Hash::make('secret'),
